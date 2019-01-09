8959647e3a78e1360410a2def8f0e8606eff9168
Roman <romaaverkin@gmail.com>
2019-01-09 16:19:19 +0700

    In Works

---
+++ app/src/main/java/com/romaaverkingmail/eggtimer/MainActivity.java
@@ -0,0 +1,83 @@
+package com.romaaverkingmail.eggtimer;
+
+import android.media.MediaPlayer;
+import android.os.CountDownTimer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView timerTextView;
+    SeekBar timerSeekBar;
+    boolean counterIsActive = false;
+    Button goButton;
+
+    public void buttonClicked(View view) {
+
+        counterIsActive = true;
+        timerSeekBar.setEnabled(false);
+        goButton.setText("STOP!");
+
+        CountDownTimer countDownTimer = new CountDownTimer(timerSeekBar.getProgress() * 1000 + 100, 500) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                Log.i("millisUntilFinished", String.valueOf(millisUntilFinished));
+                updateTimer((int) millisUntilFinished / 1000);
+            }
+
+            @Override
+            public void onFinish() {
+                MediaPlayer mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.analog_alarm_clock);
+                mediaPlayer.start();
+            }
+        }.start();
+    }
+
+    public void updateTimer(int secondsLeft) {
+        int minutes = secondsLeft / 60;
+        int seconds = secondsLeft - (minutes * 60);
+
+        String secondString = Integer.toString(seconds);
+
+        if (seconds <= 9) {
+            secondString = "0" + secondString;
+        }
+
+        timerTextView.setText(Integer.toString(minutes) + ":" + secondString);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        timerSeekBar = findViewById(R.id.timerSeekBar);
+        timerTextView = findViewById(R.id.countdownTextView);
+        goButton = findViewById(R.id.goButton);
+
+        timerSeekBar.setMax(600);
+        timerSeekBar.setProgress(30);
+
+        timerSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                updateTimer(progress);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+    }
+}
